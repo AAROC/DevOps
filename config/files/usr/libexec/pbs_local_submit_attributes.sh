b1b6aa54382df3ed5db00fb3f0aeae2849f3f33e
Paschalis Korosoglou <pkoro@grid.auth.gr>
2013-03-26 09:15:19 +0200

    added couple of reservation examples in maui.cfg

--- config/files/usr/libexec/pbs_local_submit_attributes.sh
+++ config/files/usr/libexec/pbs_local_submit_attributes.sh
@@ -2,3 +2,6 @@
 if [ "x$GlueHostApplicationSoftwareRunTimeEnvironment" == "x\"TAG1\"" ]; then
  echo "#PBS -l nodes=1:tag1:ppn=4"
 fi
+if [ "x$GlueHostApplicationSoftwareRunTimeEnvironment" == "x\"GPU\"" ]; then
+ echo "#PBS -l nodes=1:gpu"
+fi
