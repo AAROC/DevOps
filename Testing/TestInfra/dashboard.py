76f1d50dfa8492128d7c3c8658b014113d612f2c
Bruce Becker <brucellino@gmail.com>
2017-09-13 18:17:39 +0200

    Add further tests on the grafana ini file

--- Testing/TestInfra/dashboard.py
+++ Testing/TestInfra/dashboard.py
@@ -5,10 +5,16 @@ def test_package(host):
     assert host.package('grafana').is_installed
 
 def test_init(host):
-   assert host.file('/etc/grafana/grafana.ini').exists
+    file=host.file('/etc/grafana/grafana.ini')
+    assert file.exists
+    assert file.user == 'root'
+    assert file.group == 'grafana'
+    assert file.mode == 640
+
 
 
 def test_service(host):
     service=host.service('grafana-server')
     assert service.is_running
     assert service.is_enabled
+    assert host.socket('tcp://3000').is_listening
