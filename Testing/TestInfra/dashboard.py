44d89d33b7a16b7e57db03e3e38bf7e21ceec54e
Bruce Becker <brucellino@gmail.com>
2017-09-13 18:00:53 +0200

    Add service tests to grafana

--- Testing/TestInfra/dashboard.py
+++ Testing/TestInfra/dashboard.py
@@ -1,5 +1,14 @@
+# Infrastructure tests on grafana dashboards provisioned with
+# the grafana.yml playbook and grafana role.
+
 def test_package(host):
     assert host.package('grafana').is_installed
 
 def test_init(host):
    assert host.file('/etc/grafana/grafana.ini').exists
+
+
+def test_service(host):
+    service=host.service('grafana-server')
+    assert service.is_running
+    assert service.is_enabled
